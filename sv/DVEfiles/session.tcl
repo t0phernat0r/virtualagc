324d459ff6cf7d33b6bd18ba02a3030d38e9af5b
Christopher Bernard <cabernar@andrew.cmu.edu>
2022-04-05 19:27:03 -0400

    caught a lot of bugs

--- sv/DVEfiles/session.tcl
+++ sv/DVEfiles/session.tcl
@@ -1,13 +1,13 @@
 # Begin_DVE_Session_Save_Info
 # DVE full session
-# Saved on Mon Apr 4 14:51:01 2022
+# Saved on Tue Apr 5 14:13:53 2022
 # Designs open: 1
 #   Sim: /afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv/simv
 # Toplevel windows open: 1
-# 	TopLevel.2
-#   Wave.1: 16 signals
+# 	TopLevel.1
+#   Source.1: _vcs_unit__1064167090
 #   Group count = 1
-#   Group Group1 signal count = 16
+#   Group Group1 signal count = 25
 # End_DVE_Session_Save_Info
 
 # DVE version: K-2015.09_Full64
@@ -58,15 +58,15 @@ gui_src_preferences -tabstop 8 -maxbits 24 -windownumber 1
 # DVE top-level session
 
 
-# Create and position top-level window: TopLevel.2
+# Create and position top-level window: TopLevel.1
 
-if {![gui_exist_window -window TopLevel.2]} {
-    set TopLevel.2 [ gui_create_window -type TopLevel \
+if {![gui_exist_window -window TopLevel.1]} {
+    set TopLevel.1 [ gui_create_window -type TopLevel \
        -icon $::env(DVE)/auxx/gui/images/toolbars/dvewin.xpm] 
 } else { 
-    set TopLevel.2 TopLevel.2
+    set TopLevel.1 TopLevel.1
 }
-gui_show_window -window ${TopLevel.2} -show_state maximized -rect {{60 66} {2619 1402}}
+gui_show_window -window ${TopLevel.1} -show_state normal -rect {{8 149} {1546 992}}
 
 # ToolBar settings
 gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
@@ -113,23 +113,58 @@ gui_show_toolbar -toolbar {Testbench}
 # End ToolBar settings
 
 # Docked window settings
-gui_sync_global -id ${TopLevel.2} -option true
+set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 172]
+catch { set Hier.1 [gui_share_window -id ${HSPane.1} -type Hier] }
+gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 172
+gui_set_window_pref_key -window ${HSPane.1} -key dock_height -value_type integer -value -1
+gui_set_window_pref_key -window ${HSPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 171} {height 587} {dock_state left} {dock_on_new_line true} {child_hier_colhier 140} {child_hier_coltype 100} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
+set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 181]
+catch { set Data.1 [gui_share_window -id ${DLPane.1} -type Data] }
+gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 181
+gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 587
+gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 180} {height 587} {dock_state left} {dock_on_new_line true} {child_data_colvariable 140} {child_data_colvalue 100} {child_data_coltype 40} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 158]
+gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value 1476
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 158
+gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 1538} {height 157} {dock_state bottom} {dock_on_new_line true}}
+#### Start - Readjusting docked view's offset / size
+set dockAreaList { top left right bottom }
+foreach dockArea $dockAreaList {
+  set viewList [gui_ekki_get_window_ids -active_parent -dock_area $dockArea]
+  foreach view $viewList {
+      if {[lsearch -exact [gui_get_window_pref_keys -window $view] dock_width] != -1} {
+        set dockWidth [gui_get_window_pref_value -window $view -key dock_width]
+        set dockHeight [gui_get_window_pref_value -window $view -key dock_height]
+        set offset [gui_get_window_pref_value -window $view -key dock_offset]
+        if { [string equal "top" $dockArea] || [string equal "bottom" $dockArea]} {
+          gui_set_window_attributes -window $view -dock_offset $offset -width $dockWidth
+        } else {
+          gui_set_window_attributes -window $view -dock_offset $offset -height $dockHeight
+        }
+      }
+  }
+}
+#### End - Readjusting docked view's offset / size
+gui_sync_global -id ${TopLevel.1} -option true
 
 # MDI window settings
-set Wave.1 [gui_create_window -type {Wave}  -parent ${TopLevel.2}]
-gui_show_window -window ${Wave.1} -show_state maximized
-gui_update_layout -id ${Wave.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false} {child_wave_left 743} {child_wave_right 1811} {child_wave_colname 360} {child_wave_colvalue 379} {child_wave_col1 0} {child_wave_col2 1}}
+set Source.1 [gui_create_window -type {Source}  -parent ${TopLevel.1}]
+gui_show_window -window ${Source.1} -show_state maximized
+gui_update_layout -id ${Source.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false}}
 
 # End MDI window settings
 
-gui_set_env TOPLEVELS::TARGET_FRAME(Source) none
-gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) none
-gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) none
+gui_set_env TOPLEVELS::TARGET_FRAME(Source) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) ${TopLevel.1}
 gui_set_env TOPLEVELS::TARGET_FRAME(Wave) none
 gui_set_env TOPLEVELS::TARGET_FRAME(List) none
-gui_set_env TOPLEVELS::TARGET_FRAME(Memory) none
+gui_set_env TOPLEVELS::TARGET_FRAME(Memory) ${TopLevel.1}
 gui_set_env TOPLEVELS::TARGET_FRAME(DriverLoad) none
-gui_update_statusbar_target_frame ${TopLevel.2}
+gui_update_statusbar_target_frame ${TopLevel.1}
 
 #</WindowLayout>
 
@@ -138,11 +173,11 @@ gui_update_statusbar_target_frame ${TopLevel.2}
 # DVE Open design session: 
 
 if { [llength [lindex [gui_get_db -design Sim] 0]] == 0 } {
-gui_set_env SIMSETUP::SIMARGS {{-ucligui }}
-gui_set_env SIMSETUP::SIMEXE {/afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv/simv}
+gui_set_env SIMSETUP::SIMARGS {{}}
+gui_set_env SIMSETUP::SIMEXE {./simv}
 gui_set_env SIMSETUP::ALLOW_POLL {0}
 if { ![gui_is_db_opened -db {/afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv/simv}] } {
-gui_sim_run Ucli -exe simv -args {-ucligui } -dir /afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv -nosource
+gui_sim_run Ucli -exe simv -args { -ucligui} -dir /afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv -nosource
 }
 }
 if { ![gui_sim_state -check active] } {error "Simulator did not start correctly" error}
@@ -166,11 +201,11 @@ gui_set_time_units 1ps
 # Global: Signal Groups
 
 
-set _session_group_2 Group1
-gui_sg_create "$_session_group_2"
-set Group1 "$_session_group_2"
+set _session_group_1 Group1
+gui_sg_create "$_session_group_1"
+set Group1 "$_session_group_1"
 
-gui_sg_addsignal -group "$_session_group_2" { TB.core.clock TB.core.reset_n TB.core.stall_D TB.core.flush_E TB.core.flush_W TB.core.flush_DE TB.core.branch_E TB.core.ctrl_D TB.core.ctrl_E TB.core.ctrl_F TB.core.ctrl_W TB.core.pc_F TB.core.pc_1_F TB.core.next_pc_F TB.core.pc_E TB.core.pc_D }
+gui_sg_addsignal -group "$_session_group_1" { TB.core.clock TB.core.reset_n TB.core.stall_D TB.core.alu_out_E TB.core.alu_src2_E TB.core.ctrl_D TB.core.ctrl_E TB.core.ctrl_F TB.core.ctrl_W TB.core.alu_src1_E TB.core.alu_out_W TB.core.rf.rs1_data TB.core.rf.rs2_data TB.core.rf.bits_EB TB.core.rf.bits_FB TB.core.rf.reg_A TB.core.rf.reg_L TB.core.rf.reg_Q TB.core.rf.reg_BB TB.core.rf.reg_CYR TB.core.rf.reg_SR TB.core.rf.reg_CYL TB.core.rf.reg_SL TB.core.rf.reg_TIME1 TB.core.rf.reg_TIME2 }
 
 # Global: Highlighting
 
@@ -180,7 +215,7 @@ gui_change_stack_mode -mode list
 # Post database loading setting...
 
 # Restore C1 time
-gui_set_time -C1_only 95
+gui_set_time -C1_only 105
 
 
 
@@ -199,43 +234,41 @@ gui_set_loading_session_type noSession
 # DVE View/pane content session: 
 
 
-# View 'Wave.1'
-gui_wv_sync -id ${Wave.1} -switch false
-set groupExD [gui_get_pref_value -category Wave -key exclusiveSG]
-gui_set_pref_value -category Wave -key exclusiveSG -value {false}
-set origWaveHeight [gui_get_pref_value -category Wave -key waveRowHeight]
-gui_list_set_height -id Wave -height 25
-set origGroupCreationState [gui_list_create_group_when_add -wave]
-gui_list_create_group_when_add -wave -disable
-gui_marker_set_ref -id ${Wave.1}  C1
-gui_wv_zoom_timerange -id ${Wave.1} 0 597
-gui_list_add_group -id ${Wave.1} -after {New Group} {Group1}
-gui_list_expand -id ${Wave.1} TB.core.ctrl_E
-gui_seek_criteria -id ${Wave.1} {Any Edge}
+# Hier 'Hier.1'
+gui_show_window -window ${Hier.1}
+gui_list_set_filter -id ${Hier.1} -list { {Package 1} {All 0} {Process 1} {VirtPowSwitch 0} {UnnamedProcess 1} {UDP 0} {Function 1} {Block 1} {SrsnAndSpaCell 0} {OVA Unit 1} {LeafScCell 1} {LeafVlgCell 1} {Interface 1} {LeafVhdCell 1} {$unit 1} {NamedBlock 1} {Task 1} {VlgPackage 1} {ClassDef 1} {VirtIsoCell 0} }
+gui_list_set_filter -id ${Hier.1} -text {*}
+gui_hier_list_init -id ${Hier.1}
+gui_change_design -id ${Hier.1} -design Sim
+catch {gui_list_expand -id ${Hier.1} TB}
+catch {gui_list_expand -id ${Hier.1} TB.core}
+catch {gui_list_select -id ${Hier.1} {TB.core.rf}}
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
 
+# Data 'Data.1'
+gui_list_set_filter -id ${Data.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {LowPower 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
+gui_list_set_filter -id ${Data.1} -text {*}
+gui_list_show_data -id ${Data.1} {TB.core.rf}
+gui_show_window -window ${Data.1}
+catch { gui_list_select -id ${Data.1} {TB.core.rf.rs1_data TB.core.rf.rs2_data TB.core.rf.bits_EB TB.core.rf.bits_FB TB.core.rf.reg_A TB.core.rf.reg_L TB.core.rf.reg_Q TB.core.rf.reg_BB TB.core.rf.reg_CYR TB.core.rf.reg_SR TB.core.rf.reg_CYL TB.core.rf.reg_SL TB.core.rf.reg_TIME1 TB.core.rf.reg_TIME2 }}
+gui_view_scroll -id ${Data.1} -vertical -set 0
+gui_view_scroll -id ${Data.1} -horizontal -set 0
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
 
-
-gui_set_env TOGGLE::DEFAULT_WAVE_WINDOW ${Wave.1}
-gui_set_pref_value -category Wave -key exclusiveSG -value $groupExD
-gui_list_set_height -id Wave -height $origWaveHeight
-if {$origGroupCreationState} {
-	gui_list_create_group_when_add -wave -enable
-}
-if { $groupExD } {
- gui_msg_report -code DVWW028
-}
-gui_list_set_filter -id ${Wave.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
-gui_list_set_filter -id ${Wave.1} -text {*}
-gui_list_set_insertion_bar  -id ${Wave.1} -group Group1  -position in
-
-gui_marker_move -id ${Wave.1} {C1} 95
-gui_view_scroll -id ${Wave.1} -vertical -set 0
-gui_show_grid -id ${Wave.1} -enable false
+# Source 'Source.1'
+gui_src_value_annotate -id ${Source.1} -switch false
+gui_set_env TOGGLE::VALUEANNOTATE 0
+gui_open_source -id ${Source.1}  -replace -active _vcs_unit__1064167090 /afs/ece.cmu.edu/usr/cabernar/Private/capstone/virtualagc/sv/top.sv
+gui_view_scroll -id ${Source.1} -vertical -set 0
+gui_src_set_reusable -id ${Source.1}
 # Restore toplevel window zorder
 # The toplevel window could be closed if it has no view/pane
-if {[gui_exist_window -window ${TopLevel.2}]} {
-	gui_set_active_window -window ${TopLevel.2}
-	gui_set_active_window -window ${Wave.1}
+if {[gui_exist_window -window ${TopLevel.1}]} {
+	gui_set_active_window -window ${TopLevel.1}
+	gui_set_active_window -window ${Source.1}
+	gui_set_active_window -window ${DLPane.1}
 }
 #</Session>
 
--- sv/csrc/filelist.cu
+++ sv/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _7702_archive_1.so
-_7702_archive_1.so : archive.5/_7702_archive_1.a
+ARCHIVE_OBJS += _19198_archive_1.so
+_19198_archive_1.so : archive.25/_19198_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_7702_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_19198_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_7702_archive_1.so $@
+	@ln -sf .//../simv.daidir//_19198_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.5/_prev_archive_1.a
+_prev_archive_1.so : archive.25/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
